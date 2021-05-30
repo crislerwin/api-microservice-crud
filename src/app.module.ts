ceca8045a36f840c78913680cfdc91962910e011
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-05-30 13:14:24 +0000

    chore: config environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './models/user.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_DATABASE,
+      entities: [User],
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController, User],
   providers: [AppService],
 })
 export class AppModule {}
