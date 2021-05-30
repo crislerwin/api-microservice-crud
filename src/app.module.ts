8a2b56cfb0f953814a531bfe7f2af4f2a254782f
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-05-30 20:13:13 +0000

    fix: change file folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './models/user.model';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_CONNECTION as any,
-      host: process.env.TYPEORM_HOST,
-      port: parseInt(process.env.TYPEORM_PORT),
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_DATABASE,
-      entities: [User],
-    }),
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
