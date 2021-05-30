9da8120a8188668bc3be50f8f65845db6c97a3c8
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-05-30 13:15:48 +0000

    chore: define user model table

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({ name: 'user' })
-export class User {}
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  lastname: string;
+  @Column()
+  role: string;
+  @Column()
+  email: string;
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+}
